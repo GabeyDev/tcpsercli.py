69cdaa331a2c9e1ceb06db475474e1ea2ef29dd4
Gabey <130712384+GabeyDev@users.noreply.github.com>
2024-09-13 17:09:28 -0300

    Update server2.py

--- server2.py
+++ server2.py
@@ -1,57 +1,29 @@
-import socket
-
-def identificar_pares_hexadecimal(pacote_hex):
-    start_bit = '7878'
-    end_bit = '0d0a'
+def parse_hex_strings(hex_string):
+    hex_string = hex_string.lower().replace(" ", "")
+    if len(hex_string) % 2 != 0:
+        raise ValueError("A string Hex tem que ter um número de dígitos específico")
     
-    pos_start = pacote_hex.find(start_bit)
-    print(f"Posição do start bit ('{start_bit}'): {pos_start}")
+    byte_array = bytearray.fromhex(hex_string)
+    protocol_number = byte_array[1]
     
-    if pos_start == -1:
-        return {'start_bit': '7878', 'pos_start_bit': 0, 'end_bit': '0D0A', 'pos_end_bit': 32}
-    
-    pos_end = pacote_hex.find(end_bit)
-    print(f"Posição do end bit ('{end_bit}'): {pos_end}")
-    
-    if pos_end == -1:
-        return {'start_bit': pos_start, 'pos_start_bit': len(start_bit) // 2, 'end_bit': '0D0A', 'pos_end_bit': 32}
-    
-   
-    pos_start_bit = len(start_bit) // 2
-    pos_end_bit = len(end_bit) // 2
-    
-    return {
-        'start_bit': pos_start,
-        'pos_start_bit': pos_start_bit,
-        'end_bit': pos_end,
-        'pos_end_bit': pos_start_bit
+    common_fields = {
+        "Start bit": byte_array[:2].hex(),
+        "Packet Length": f"0x{byte_array[2]:02X}",
+        "Protocol Number": f"0x{protocol_number:02X}",
+        "Information Serial Number": byte_array[13:15].hex(),
+        "Error Check": byte_array[16:18].hex(),
+        "End Bit": byte_array[19:21].hex()
     }
-
-def main():
-    host = '127.0.0.1'
-    port = 5050
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen()
-
-        print(f"Servidor TCP ouvindo o host {host}: e o port {port}")
-
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                print(f"Conectado por {addr}")
-                data = conn.recv(1024)
-                
-                if data:
-                    pacote_hex = data.hex().upper()
-                    print(f"Pacote recebido: {pacote_hex}")
-                    
-                    resultado = identificar_pares_hexadecimal(pacote_hex)
-                    print(f"Resultado da identificação: {resultado}")
-                    
-                    resposta = str(resultado).encode('utf-8')
-                    conn.sendall(resposta)
-
-if __name__ == "__main__":
-    main()
+    
+    specific_field = {}
+    
+    # Login
+    if protocol_number == 0x01:
+        specific_field = {
+            "Device ID": byte_array[4:12].hex()
+        }
+    else:
+        raise ValueError("PAcote desconhecido.")
+    
+    common_fields.update(specific_field)
+    return common_fields
